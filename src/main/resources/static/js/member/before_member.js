181a54c3997d9c70d31668f0225758da22a9add9
kristal-ddp <kristal.ddp@gmail.com>
2024-01-15 23:09:21 +0900

    🎨 Make Input Data Persist

--- src/main/resources/static/js/member/before_member.js
+++ src/main/resources/static/js/member/before_member.js
@@ -1,20 +1,21 @@
 // 회원가입 중 생년월일
-$(document).ready(function () {
+//$(document).ready(function () {
+//
+//    // 년도, 월, 일 옵션 설정
+//    function options(select, start, end) {
+//        for (let i = start; i <= end; i++) {
+//            select.append($('<option>', {
+//                value: i,
+//                text: i
+//            }));
+//        }
+//    }
+//
+//    options($('select[name="year"]'), 1900, 2025); // Change the range as needed
+//    options($('select[name="month"]'), 1, 12);
+//    options($('select[name="day"]'), 1, 31);
+//});
 
-    // 년도, 월, 일 옵션 설정
-    function options(select, start, end) {
-        for (let i = start; i <= end; i++) {
-            select.append($('<option>', {
-                value: i,
-                text: i
-            }));
-        }
-    }
-
-    options($('select[name="year"]'), 1900, 2025); // Change the range as needed
-    options($('select[name="month"]'), 1, 12);
-    options($('select[name="day"]'), 1, 31);
-});
 
 // 회원가입 중 생년월일 미선택시 오류 발생
 //$(document).ready(function () {
